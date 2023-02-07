b2db6cda28c307f18e9be49de0fad18c57a98dcc
Daniel Bakhtiari <61508554+DanielBakhtiari@users.noreply.github.com>
2023-02-07 16:22:06 +0100

    Added Rock paper scissor

---
+++ Rock paper scissors/README.md
@@ -0,0 +1,3 @@
+# Rock Paper Scissors
+
+This is an website with the game rock paper and scissors. This site has been created by using HTML, CSS and Javascript.
---
+++ Rock paper scissors/script.js
@@ -0,0 +1,70 @@
+let compscore = 0;
+let playerscore = 0;
+const rockbut = document.querySelector("#rock");
+const paperbut = document.querySelector("#paper");
+const scissorbut = document.querySelector("#scissor");
+const comp = document.querySelector("#computerchoice");
+const cs = document.querySelector("#computerscore");
+const ps = document.querySelector("#playerscore");
+const winner = document.querySelector("#winner");
+
+function getcomputerchoice() {
+    const list = ["rock","paper","scissor"];
+    return list[Math.floor(Math.random() * 3)];
+}
+    
+function playround(playerchoice,computerchoice) {
+    winner.textContent = ""
+    comp.textContent = computerchoice;
+    if (playerchoice == "rock" && computerchoice == "paper") {
+        compscore += 1;
+        cs.textContent = compscore;
+        gameend()
+    } else if (playerchoice == "paper" && computerchoice == "scissor") {
+        compscore += 1;
+        cs.textContent = compscore;
+        gameend();
+    } else if (playerchoice == "scissor" && computerchoice == "rock") {
+        compscore += 1;
+        cs.textContent = compscore;
+        gameend()
+    } 
+    else if (playerchoice ==  computerchoice ) {
+    } 
+    else {
+        playerscore += 1;
+        ps.textContent = playerscore;
+        gameend()
+        return "You win " + playerchoice +  " beats " + computerchoice;
+    }
+}
+
+function gameend() {
+    if (playerscore == 5) {
+        playerscore = 0;
+        compscore = 0;
+        cs.textContent = "0";
+        ps.textContent = "0";
+        comp.textContent = "";
+        winner.textContent = "Player Won!"
+    }
+    if (compscore == 5) {
+        playerscore = 0;
+        compscore = 0;
+        cs.textContent = "0";
+        ps.textContent = "0";
+        comp.textContent = "";
+        winner.textContent = "Computer Won!"
+        
+    } 
+}
+
+rockbut.addEventListener("click", function() {
+    console.log(playround("rock", getcomputerchoice()));
+});
+paperbut.addEventListener("click", function() {
+    console.log(playround("paper", getcomputerchoice()));
+});
+scissorbut.addEventListener("click", function() {
+    console.log(playround("scissor", getcomputerchoice()));
+});
